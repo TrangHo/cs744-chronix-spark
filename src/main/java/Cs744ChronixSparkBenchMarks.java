d02c86faa9d5909755d4360bd2d932a7016174b0
TrangHo <carepjan@KuroiTamago.local>
2017-11-15 16:51:47 -0600

    Init commit for chronix spark program with gradle

---
+++ src/main/java/Cs744ChronixSparkBenchMarks.java
@@ -0,0 +1,10 @@
+import de.qaware.chronix.spark.api.java.ChronixSparkContext;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.solr.client.solrj.SolrQuery;
+import de.qaware.chronix.storage.solr.ChronixSolrCloudStorage;
+
+public class Cs744ChronixSparkBenchMarks {
+  public static void main( String[] args ) {
+    System.out.println( "Hello World!" );
+  }
+}
