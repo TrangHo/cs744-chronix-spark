162041901d7bcf3615e9c1923b8c5d48c8654acf
TrangHo <carepjan@KuroiTamago.local>
2017-11-15 19:57:08 -0600

    Try running simple query (min) on chronix spark

--- src/main/java/Cs744ChronixSparkBenchMarks.java
+++ src/main/java/Cs744ChronixSparkBenchMarks.java
@@ -8,7 +8,7 @@ import de.qaware.chronix.storage.solr.ChronixSolrCloudStorage;
 public class Cs744ChronixSparkBenchMarks {
   public static void main( String[] args ) {
     // Create ChronixSparkContext from a SparkContext/JavaSparkContext
-    SparkConf conf = new SparkConf();
+    SparkConf conf = new SparkConf().setAppName("CS744 Chronix Spark Benchmarks");
     ChronixSparkContext csc = new ChronixSparkContext(new JavaSparkContext(conf));
     SolrQuery query = new SolrQuery("metric:\"java.lang:type=Memory/HeapMemoryUsage/used\"");
     ChronixRDD rdd;
@@ -17,6 +17,7 @@ public class Cs744ChronixSparkBenchMarks {
                         "http://128.104.222.123:2181", // Zookeeper host
                         "jp_us_exchange_rates", // Solr Collection for Chronix
                         new ChronixSolrCloudStorage());
+      System.out.println("MIN: " + rdd.min());
     } catch (Exception e) {
       System.out.println(e);
     }
